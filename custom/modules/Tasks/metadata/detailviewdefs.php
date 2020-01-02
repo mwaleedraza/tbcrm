dda484958642d7137ef512f15767835feb7cb69f
Zubairkhalid0070 <khalidzubair641@gmail.com>
2020-01-02 17:58:54 +0500

    task module layout chnages

---
+++ custom/modules/Tasks/metadata/detailviewdefs.php
@@ -0,0 +1,175 @@
+<?php
+$viewdefs ['Tasks'] = 
+array (
+  'DetailView' => 
+  array (
+    'templateMeta' => 
+    array (
+      'form' => 
+      array (
+        'buttons' => 
+        array (
+          0 => 'EDIT',
+          1 => 'DUPLICATE',
+          2 => 'DELETE',
+          3 => 
+          array (
+            'customCode' => '{if $fields.status.value != "Completed"} <input type="hidden" name="isSaveAndNew" value="false">  <input type="hidden" name="status" value="">  <input id="close_and_create_new_button" title="{$APP.LBL_CLOSE_AND_CREATE_BUTTON_TITLE}"  class="button"  onclick="this.form.action.value=\'Save\'; this.form.return_module.value=\'Tasks\'; this.form.isDuplicate.value=true; this.form.isSaveAndNew.value=true; this.form.return_action.value=\'EditView\'; this.form.isDuplicate.value=true; this.form.return_id.value=\'{$fields.id.value}\';"  name="button"  value="{$APP.LBL_CLOSE_AND_CREATE_BUTTON_TITLE}"  type="submit">{/if}',
+            'sugar_html' => 
+            array (
+              'type' => 'submit',
+              'value' => '{$APP.LBL_CLOSE_AND_CREATE_BUTTON_TITLE}',
+              'htmlOptions' => 
+              array (
+                'title' => '{$APP.LBL_CLOSE_AND_CREATE_BUTTON_TITLE}',
+                'class' => 'button',
+                'onclick' => 'this.form.action.value=\'Save\'; this.form.return_module.value=\'Tasks\'; this.form.isDuplicate.value=true; this.form.isSaveAndNew.value=true; this.form.return_action.value=\'EditView\'; this.form.isDuplicate.value=true; this.form.return_id.value=\'{$fields.id.value}\';',
+                'name' => 'button',
+                'id' => 'close_and_create_new_button',
+              ),
+              'template' => '{if $fields.status.value != "Completed"}[CONTENT]{/if}',
+            ),
+          ),
+          4 => 
+          array (
+            'customCode' => '{if $fields.status.value != "Completed"} <input type="hidden" name="isSave" value="false">  <input title="{$APP.LBL_CLOSE_BUTTON_TITLE}" id="close_button" class="button"  onclick="this.form.status.value=\'Completed\'; this.form.action.value=\'Save\';this.form.return_module.value=\'Tasks\';this.form.isSave.value=true;this.form.return_action.value=\'DetailView\'; this.form.return_id.value=\'{$fields.id.value}\'"  name="button1"  value="{$APP.LBL_CLOSE_BUTTON_TITLE}"  type="submit">{/if}',
+            'sugar_html' => 
+            array (
+              'type' => 'submit',
+              'value' => '{$APP.LBL_CLOSE_BUTTON_TITLE}',
+              'htmlOptions' => 
+              array (
+                'title' => '{$APP.LBL_CLOSE_BUTTON_TITLE}',
+                'class' => 'button',
+                'onclick' => 'this.form.status.value=\'Completed\'; this.form.action.value=\'Save\';this.form.return_module.value=\'Tasks\';this.form.isSave.value=true;this.form.return_action.value=\'DetailView\'; this.form.return_id.value=\'{$fields.id.value}\'',
+                'name' => 'button1',
+                'id' => 'close_button',
+              ),
+              'template' => '{if $fields.status.value != "Completed"}[CONTENT]{/if}',
+            ),
+          ),
+        ),
+        'hidden' => 
+        array (
+          0 => '<input type="hidden" name="isSaveAndNew">',
+          1 => '<input type="hidden" name="status" value="">',
+          2 => '<input type="hidden" name="isSave">',
+        ),
+      ),
+      'maxColumns' => '2',
+      'widths' => 
+      array (
+        0 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+        1 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+      ),
+      'useTabs' => false,
+      'tabDefs' => 
+      array (
+        'LBL_TASK_INFORMATION' => 
+        array (
+          'newTab' => false,
+          'panelDefault' => 'expanded',
+        ),
+      ),
+    ),
+    'panels' => 
+    array (
+      'lbl_task_information' => 
+      array (
+        0 => 
+        array (
+          0 => 
+          array (
+            'name' => 'serial_no',
+            'label' => 'LBL_SERIAL_NO',
+          ),
+          1 => '',
+        ),
+        1 => 
+        array (
+          0 => 
+          array (
+            'name' => 'name',
+            'label' => 'LBL_SUBJECT',
+          ),
+          1 => 
+          array (
+            'name' => 'category',
+            'comment' => '',
+            'label' => 'LBL_CATEGORY',
+          ),
+        ),
+        2 => 
+        array (
+          0 => 'date_start',
+          1 => 'date_due',
+        ),
+        3 => 
+        array (
+          0 => 'status',
+          1 => 
+          array (
+            'name' => 'contact_name',
+            'label' => 'LBL_CONTACT',
+          ),
+        ),
+        4 => 
+        array (
+          0 => 
+          array (
+            'name' => 'assigned_user_name',
+            'label' => 'LBL_ASSIGNED_TO',
+          ),
+          1 => 'priority',
+        ),
+        5 => 
+        array (
+          0 => 'description',
+          1 => 
+          array (
+            'name' => 'next_action',
+            'label' => 'LBL_NEXT_ACTION',
+          ),
+        ),
+        6 => 
+        array (
+          0 => 
+          array (
+            'name' => 'comment',
+            'label' => 'LBL_COMMENT',
+          ),
+          1 => 
+          array (
+            'name' => 'closure_date',
+            'label' => 'LBL_CLOSURE_DATE',
+          ),
+        ),
+        7 => 
+        array (
+          0 => 
+          array (
+            'name' => 'date_entered',
+            'customCode' => '{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
+            'label' => 'LBL_DATE_ENTERED',
+          ),
+          1 => 
+          array (
+            'name' => 'date_modified',
+            'customCode' => '{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
+            'label' => 'LBL_DATE_MODIFIED',
+          ),
+        ),
+      ),
+    ),
+  ),
+);
+;
+?>
---
+++ custom/modules/Tasks/metadata/searchdefs.php
@@ -0,0 +1,126 @@
+<?php
+$searchdefs ['Tasks'] = 
+array (
+  'layout' => 
+  array (
+    'basic_search' => 
+    array (
+      'name' => 
+      array (
+        'name' => 'name',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'current_user_only' => 
+      array (
+        'name' => 'current_user_only',
+        'label' => 'LBL_CURRENT_USER_FILTER',
+        'type' => 'bool',
+        'default' => true,
+        'width' => '10%',
+      ),
+      0 => 
+      array (
+        'name' => 'open_only',
+        'label' => 'LBL_OPEN_ITEMS',
+        'type' => 'bool',
+        'default' => false,
+        'width' => '10%',
+      ),
+      1 => 
+      array (
+        'name' => 'favorites_only',
+        'label' => 'LBL_FAVORITES_FILTER',
+        'type' => 'bool',
+      ),
+    ),
+    'advanced_search' => 
+    array (
+      'name' => 
+      array (
+        'name' => 'name',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'contact_name' => 
+      array (
+        'name' => 'contact_name',
+        'label' => 'LBL_CONTACT_NAME',
+        'type' => 'name',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'current_user_only' => 
+      array (
+        'name' => 'current_user_only',
+        'label' => 'LBL_CURRENT_USER_FILTER',
+        'type' => 'bool',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'status' => 
+      array (
+        'name' => 'status',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'category' => 
+      array (
+        'type' => 'enum',
+        'label' => 'LBL_CATEGORY',
+        'width' => '10%',
+        'default' => true,
+        'name' => 'category',
+      ),
+      'assigned_user_id' => 
+      array (
+        'name' => 'assigned_user_id',
+        'type' => 'enum',
+        'label' => 'LBL_ASSIGNED_TO',
+        'function' => 
+        array (
+          'name' => 'get_user_array',
+          'params' => 
+          array (
+            0 => false,
+          ),
+        ),
+        'default' => true,
+        'width' => '10%',
+      ),
+      'date_entered' => 
+      array (
+        'type' => 'datetime',
+        'label' => 'LBL_DATE_ENTERED',
+        'width' => '10%',
+        'default' => true,
+        'name' => 'date_entered',
+      ),
+      'date_start' => 
+      array (
+        'type' => 'datetimecombo',
+        'studio' => 
+        array (
+          'required' => true,
+          'no_duplicate' => true,
+        ),
+        'label' => 'LBL_START_DATE',
+        'width' => '10%',
+        'default' => true,
+        'name' => 'date_start',
+      ),
+    ),
+  ),
+  'templateMeta' => 
+  array (
+    'maxColumns' => '3',
+    'maxColumnsBasic' => '4',
+    'widths' => 
+    array (
+      'label' => '10',
+      'field' => '30',
+    ),
+  ),
+);
+;
+?>
