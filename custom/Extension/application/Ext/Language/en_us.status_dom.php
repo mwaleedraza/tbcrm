9c5ff2e6132cd1b48fc945df84ce50b52dc0f0d5
hamzzzakhan <hamzakhan7003@yahoo.com>
2020-01-07 11:56:52 +0500

    invoice status dom updated

---
+++ custom/Extension/application/Ext/Language/en_us.status_dom.php
@@ -0,0 +1,10 @@
+<?php
+
+$app_list_strings['status_dom'] = array(
+  '' => '',
+  'Raised' => 'Raised',
+  'Not Raised' => 'Not Raised',
+  'On Hold' => 'On Hold',
+  'Cancelled' => 'Cancelled',
+);
+?>
