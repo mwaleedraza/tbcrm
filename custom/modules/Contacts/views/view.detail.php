129fb064003f2a527c168c4010e13afc80d34354
MuhammadHamzaShahid <busbuchoo@gmail.com>
2020-01-07 18:05:10 +0500

    Button hide

---
+++ custom/modules/Contacts/views/view.detail.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+class ContactsViewDetail extends ViewDetail
+{
+
+
+
+    public function display()
+    {
+        unset($this->dv->defs['templateMeta']['form']['buttons'][4]);
+
+        parent::display();
+    }
+
+
+
+
+}
