3bbde3454d2dbfb8c77332d8dd635cb3e941ef33
L1f15bscs0299 <numanirfan760@gmail.com>
2020-01-07 17:40:40 +0500

    Leads dropdown unset

--- custom/modules/Leads/views/view.detail.php
+++ custom/modules/Leads/views/view.detail.php
@@ -11,6 +11,9 @@ class customLeadsViewDetail extends LeadsViewDetail
       $this->useModuleQuickCreateTemplate = true;
   }
   public function display(){
+    unset($this->dv->defs['templateMeta']['form']['buttons'][3]);
+    unset($this->dv->defs['templateMeta']['form']['buttons'][4]);
+    unset($this->dv->defs['templateMeta']['form']['buttons'][5]);
     global $db;
     $leadProduct = $db->query("SELECT aos_product_id FROM tc_leads_products WHERE lead_id='".$this->bean->id."' ");
     while($row = $db->fetchByAssoc($leadProduct)){
