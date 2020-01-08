a53b6a2d1958db6845f5fe05f218262639ebadff
MuhammadHamzaShahid <busbuchoo@gmail.com>
2020-01-08 13:53:32 +0500

    lead products saving fixes

--- custom/modules/Leads/beforeSave.php
+++ custom/modules/Leads/beforeSave.php
@@ -2,17 +2,17 @@
 if (!defined('sugarEntry')|| !sugarEntry){
   die('Not A Valid Entry Point');
 }
-class Save
-{
-  public function SaveAll(&$bean, $event, $arguments)
-  {
+class Save{
+  public function SaveAll(&$bean, $event, $arguments){
+	 global $db;
+	$db->query('delete from tc_leads_products where lead_id="'.$bean->id.'"');
     for($i=0; $i<$_REQUEST['product_count']; $i++){
       $module='tc_leads_products';
       $bean1 = BeanFactory::newBean($module);
       $bean1->lead_id=$bean->id;
       $bean1->aos_product_id=$_REQUEST['pro_id_'.$i];
-      $bean1->save();
-
+	  if($_REQUEST['pro_id_'.$i]!='')
+		$bean1->save();
     }
   }
 }
