7cff79fb05bacfc4ea3d011ad87e2aa792b77a3f
MuhammadHamzaShahid <busbuchoo@gmail.com>
2020-01-10 14:08:22 +0500

    Contact Relation Field for Leads Module

---
+++ custom/Extension/modules/Leads/Ext/Language/en_us.sugarfield_relation.php
@@ -0,0 +1,2 @@
+<?php
+$mod_strings['LBL_CONTACTS_NAME'] = 'Client Name:';
---
+++ custom/Extension/modules/Leads/Ext/Vardefs/contactNameRelationField.php
@@ -0,0 +1,47 @@
+<?php  
+$dictionary['Lead']['fields']['contacts_id'] =  array(
+		    'name' => 'contacts_id',
+		    'rname' => 'id',
+		    'vname' => 'LBL_CONTACTS_ID',
+		    'type' => 'id',
+		    'table' => 'contacts',
+		    'isnull' => 'true',
+		    'module' => 'Contacts',
+		    'dbType' => 'id',
+		    'reportable' => false,
+		    'audited' => true,
+		    'duplicate_merge' => 'disabled',
+        );
+$dictionary['Lead']['fields']['contacts_name'] =  array(
+            'name' => 'contacts_name',
+            'rname' => 'name',
+            'id_name' => 'contacts_id',
+            'vname' => 'LBL_CONTACTS_NAME',
+            'type' => 'relate',
+            'link' => 'contacts',
+            'table' => 'contacts',
+            'isnull' => 'true',
+            'module' => 'Contacts',
+            'source' => 'non-db',
+            'required' => true,
+        );
+$dictionary['Lead']['fields']['contacts'] =  array(
+            'name' => 'contacts',
+            'type' => 'link',
+            'relationship' => 'contacts_leads',
+            'module' => 'Contacts',
+            'bean_name' => 'Contact',
+            'source' => 'non-db',
+            'vname' => 'LBL_CONTACTS',
+        );
+$dictionary['Lead']['relationships']['contacts_leads'] =  array(
+		    'lhs_module' => 'Contacts',
+		    'lhs_table' => 'contacts',
+		    'lhs_key' => 'id',
+		    'rhs_module' => 'Leads',
+		    'rhs_table' => 'leads',
+		    'rhs_key' => 'contacts_id',
+		    'relationship_type' => 'one-to-many'
+        );
+
+?>
