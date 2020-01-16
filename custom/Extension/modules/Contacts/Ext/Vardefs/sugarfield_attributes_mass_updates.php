25560512ce2e134cb8d451e86e01b35296ee4663
MuhammadHamzaShahid <busbuchoo@gmail.com>
2020-01-16 16:15:47 +0500

    mass updated and search filters changed

---
+++ custom/Extension/modules/Contacts/Ext/Vardefs/sugarfield_attributes_mass_updates.php
@@ -0,0 +1,9 @@
+<?php
+$dictionary["Contact"]["fields"]["lawful_basis_source"]['massupdate'] = false;
+$dictionary["Contact"]["fields"]["lawful_basis"]['massupdate'] = false;
+$dictionary["Contact"]["fields"]["lead_source"]['massupdate'] = false;
+$dictionary["Contact"]["fields"]["opportunity_role"]['massupdate'] = false;
+$dictionary["Contact"]["fields"]["sync_contact"]['massupdate'] = false;
+$dictionary["Contact"]["fields"]["do_not_call"]['massupdate'] = false;
+$dictionary["Contact"]["fields"]["date_reviewed"]['massupdate'] = false;
+$dictionary["Contact"]["fields"]["optout_primary"]['massupdate'] = false;
