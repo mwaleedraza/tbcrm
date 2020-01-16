fdd17314e21070139b0e30f92fe14f3207891426
l1f15bscs0299 <numanirfan760@gmail.com>
2020-01-16 15:58:13 +0500

    leads detailview name correct qoutes to quotes

--- custom/modules/Leads/metadata/detailviewdefs.php
+++ custom/modules/Leads/metadata/detailviewdefs.php
@@ -1,22 +1,22 @@
 <?php
-$viewdefs ['Leads'] =
+$viewdefs ['Leads'] = 
 array (
-  'DetailView' =>
+  'DetailView' => 
   array (
-    'templateMeta' =>
+    'templateMeta' => 
     array (
-      'form' =>
+      'form' => 
       array (
-        'buttons' =>
+        'buttons' => 
         array (
-          'SEND_CONFIRM_OPT_IN_EMAIL' =>
+          'SEND_CONFIRM_OPT_IN_EMAIL' => 
           array (
             'customCode' => '<input type="submit" class="button hidden" disabled="disabled" title="{$APP.LBL_SEND_CONFIRM_OPT_IN_EMAIL}" onclick="this.form.return_module.value=\'Leads\'; this.form.return_action.value=\'Leads\'; this.form.return_id.value=\'{$fields.id.value}\'; this.form.action.value=\'sendConfirmOptInEmail\'; this.form.module.value=\'Leads\'; this.form.module_tab.value=\'Leads\';" name="send_confirm_opt_in_email" value="{$APP.LBL_SEND_CONFIRM_OPT_IN_EMAIL}"/>',
-            'sugar_html' =>
+            'sugar_html' => 
             array (
               'type' => 'submit',
               'value' => '{$APP.LBL_SEND_CONFIRM_OPT_IN_EMAIL}',
-              'htmlOptions' =>
+              'htmlOptions' => 
               array (
                 'class' => 'button hidden',
                 'id' => 'send_confirm_opt_in_email',
@@ -30,14 +30,14 @@ array (
           0 => 'EDIT',
           1 => 'DUPLICATE',
           2 => 'DELETE',
-          3 =>
+          3 => 
           array (
             'customCode' => '{if $bean->aclAccess("edit") && !$DISABLE_CONVERT_ACTION}<input title="{$MOD.LBL_CONVERTLEAD_TITLE}" accessKey="{$MOD.LBL_CONVERTLEAD_BUTTON_KEY}" type="button" class="button" onClick="document.location=\'index.php?module=Leads&action=ConvertLead&record={$fields.id.value}\'" name="convert" value="{$MOD.LBL_CONVERTLEAD}">{/if}',
-            'sugar_html' =>
+            'sugar_html' => 
             array (
               'type' => 'button',
               'value' => '{$MOD.LBL_CONVERTLEAD}',
-              'htmlOptions' =>
+              'htmlOptions' => 
               array (
                 'title' => '{$MOD.LBL_CONVERTLEAD_TITLE}',
                 'accessKey' => '{$MOD.LBL_CONVERTLEAD_BUTTON_KEY}',
@@ -50,14 +50,14 @@ array (
             ),
           ),
           4 => 'FIND_DUPLICATES',
-          5 =>
+          5 => 
           array (
             'customCode' => '<input title="{$APP.LBL_MANAGE_SUBSCRIPTIONS}" class="button" onclick="this.form.return_module.value=\'Leads\'; this.form.return_action.value=\'DetailView\';this.form.return_id.value=\'{$fields.id.value}\'; this.form.action.value=\'Subscriptions\'; this.form.module.value=\'Campaigns\'; this.form.module_tab.value=\'Leads\';" type="submit" name="Manage Subscriptions" value="{$APP.LBL_MANAGE_SUBSCRIPTIONS}">',
-            'sugar_html' =>
+            'sugar_html' => 
             array (
               'type' => 'submit',
               'value' => '{$APP.LBL_MANAGE_SUBSCRIPTIONS}',
-              'htmlOptions' =>
+              'htmlOptions' => 
               array (
                 'title' => '{$APP.LBL_MANAGE_SUBSCRIPTIONS}',
                 'class' => 'button',
@@ -67,11 +67,11 @@ array (
               ),
             ),
           ),
-          6 =>
+          6 => 
           array (
             'customCode' => '<input type="button" id="verifyButton" class="verifyButtonContainer" name="verify" value="Create Quotes" onClick="document.location=\'index.php?module=AOS_Quotes&action=EditView&lead_id={$fields.id.value}\'">',
           ),
-          'AOS_GENLET' =>
+          'AOS_GENLET' => 
           array (
             'customCode' => '<input type="button" class="button" onClick="showPopup();" value="{$APP.LBL_PRINT_AS_PDF}">',
           ),
@@ -80,123 +80,118 @@ array (
         'footerTpl' => 'custom/modules/Leads/tpls/productsDetailView.tpl',
       ),
       'maxColumns' => '2',
-      'widths' =>
+      'widths' => 
       array (
-        0 =>
+        0 => 
         array (
           'label' => '10',
           'field' => '30',
         ),
-        1 =>
+        1 => 
         array (
           'label' => '10',
           'field' => '30',
         ),
       ),
-      'includes' =>
+      'includes' => 
       array (
-        0 =>
+        0 => 
         array (
           'file' => 'modules/Leads/Lead.js',
         ),
       ),
       'useTabs' => false,
-      'tabDefs' =>
+      'tabDefs' => 
       array (
-        'LBL_CONTACT_INFORMATION' =>
+        'LBL_CONTACT_INFORMATION' => 
         array (
           'newTab' => false,
           'panelDefault' => 'expanded',
         ),
       ),
     ),
-    'panels' =>
+    'panels' => 
     array (
-      'LBL_CONTACT_INFORMATION' =>
+      'LBL_CONTACT_INFORMATION' => 
       array (
-        0 =>
+        0 => 
         array (
-          0 =>
+          0 => 
           array (
             'name' => 'last_name',
             'comment' => 'Last name of the contact',
             'label' => 'LBL_LAST_NAME',
           ),
-          1 =>
+        ),
+        1 => 
+        array (
+          0 => 
           array (
             'name' => 'contacts_name',
             'label' => 'LBL_CONTACTS_NAME',
           ),
+          1 => 
+          array (
+            'name' => 'accounts_name',
+            'label' => 'LBL_ACCOUNTS_NAME',
+          ),
         ),
-        1 =>
+        2 => 
         array (
-          0 =>
+          0 => 
           array (
             'name' => 'sugarfield_amount',
             'label' => 'LBL_SUGARFIELD_AMOUNT',
           ),
-          1 =>
+          1 => 
           array (
             'name' => 'sugarfield_type',
             'label' => 'LBL_SUGARFIELD_TYPE',
           ),
         ),
-        2 =>
+        3 => 
         array (
-          0 =>
+          0 => 
           array (
             'name' => 'sugarfield_priority',
             'label' => 'LBL_SUGARFIELD_PRIORITY',
           ),
-          1 =>
+          1 => 
           array (
             'name' => 'sugarfield_closuredate',
             'label' => 'LBL_SUGARFIELD_CLOSUREDATE',
           ),
         ),
-        3 =>
+        4 => 
         array (
-          0 =>
+          0 => 
           array (
-            'name' => 'sugarfield_responsible',
-            'label' => 'LBL_SUGARFIELD_RESPONSIBLE',
+            'name' => 'alt_address_postalcode',
+            'comment' => 'Postal code for alternate address',
+            'label' => 'LBL_ALT_ADDRESS_POSTALCODE',
           ),
-          1 =>
+          1 => 
           array (
             'name' => 'assigned_user_name',
             'label' => 'LBL_ASSIGNED_TO',
           ),
         ),
-        4 =>
+        5 => 
         array (
-          0 =>
+          0 => 
           array (
             'name' => 'sugarfield_status',
             'label' => 'LBL_SUGARFIELD_status',
           ),
           1 => 'lead_source',
         ),
-        5 =>
+        6 => 
         array (
           0 => 'description',
-          1 =>
+          1 => 
           array (
             'name' => 'sugarfield_nextaction',
             'label' => 'LBL_SUGARFIELD_NEXTACTION',
-          ),
-        ),
-        6 =>
-        array (
-          0 =>
-          array (
-            'name' => 'date_entered',
-            'customCode' => '{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
-          ),
-          1 =>
-          array (
-            'name' => 'date_modified',
-            'label' => 'LBL_DATE_MODIFIED',
-            'customCode' => '{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
           ),
         ),
       ),
