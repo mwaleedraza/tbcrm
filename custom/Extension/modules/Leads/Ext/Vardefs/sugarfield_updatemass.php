0a899d17db0b9ce4b9fc1fb4c9ca24ccda473290
l1f15bscs0299 <numanirfan760@gmail.com>
2020-01-16 16:34:36 +0500

    leads mass update

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_updatemass.php
@@ -0,0 +1,5 @@
+<?php
+$dictionary["Lead"]["fields"]["lawful_basis_source"]['massupdate'] = false;
+$dictionary["Lead"]["fields"]["lawful_basis"]['massupdate'] = false;
+$dictionary["Lead"]["fields"]["do_not_call"]['massupdate'] = false;
+$dictionary["Lead"]["fields"]["date_reviewed"]['massupdate'] = false;
