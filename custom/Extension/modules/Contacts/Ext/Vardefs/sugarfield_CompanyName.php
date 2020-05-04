b5c6f48d246d193a6128c77f54b424362305044c
hamzzzakhan <hamzakhan7003@yahoo.com>
2020-05-04 18:03:57 +0500

    deleted extra company name field from contacts

--- custom/Extension/modules/Contacts/Ext/Vardefs/sugarfield_CompanyName.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dictionary["Contact"]["fields"]["company_name"] = array(
-    'name' => 'company_name',
-    'vname' => 'LBL_COMPANY_NAME',
-    'type' => 'varchar',
-    'len' => '20',
-    'importable' => 'true',
-);
