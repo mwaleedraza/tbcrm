80a0c7a2660aca7910fcab5c60cd6c3b4ffc3dd1
hamzzzakhan <hamzakhan7003@yahoo.com>
2020-07-17 01:59:19 +0500

    get user billing id updated in aos quotes

--- custom/modules/AOS_Quotes/GetUserBillingId.php
+++ custom/modules/AOS_Quotes/GetUserBillingId.php
@@ -7,9 +7,7 @@ global $timedate;
 $id = $_REQUEST['id'];
 $bean = BeanFactory::getBean('Contacts', $_REQUEST['id']);
 $aos =  BeanFactory::getBean('AOS_Quotes', '587bddb1-e112-3294-dc9b-5f102c94c592');
-echo "<pre>";
-print_r($aos);
-die();
+
 if ($bean->load_relationship('aos_quotes')) {
     $relatedBeans = $bean->aos_quotes->getBeans();
     $result = $bean->quotenumber;
