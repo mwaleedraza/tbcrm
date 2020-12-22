0656fc27d8acc6c2047792b8daeae6648af88135
SaimMoazzam <saimqwerty007@gmail.com>
2020-12-22 16:43:21 +0500

    quotes fixes

---
+++ custom/Extension/application/Ext/Language/en_us.approval_status_dom.php
@@ -0,0 +1,5 @@
+<?php
+$app_list_strings['approval_status_dom'] = array(
+    'Not Approved' => 'Not Approved',
+    'Approved' => 'Approved',
+);
---
+++ custom/Extension/modules/AOS_Quotes/Ext/Vardefs/sugarfield_pra_tax.php
@@ -0,0 +1,17 @@
+<?php
+$dictionary["AOS_Quotes"]["fields"]["pra_tax"] = array (
+    'name' => 'pra_tax',
+    'vname' => 'LBL_PRA_TAX',
+    'type' => 'varchar',
+    'required' => false,
+	'massupdate' => 0,
+	'no_default' => false,
+	'importable' => 'true',
+	'duplicate_merge' => 'disabled',
+	'duplicate_merge_dom_value' => '0',
+	'audited' => false,
+	'reportable' => true,
+	'unified_search' => false,
+	'merge_filter' => 'disabled',
+	'inline_edit' => '1',
+);
