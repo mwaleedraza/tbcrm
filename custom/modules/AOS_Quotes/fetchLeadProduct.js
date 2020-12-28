746a9f47460e185d6a1593c08a16272509955673
SaimMoazzam <saimqwerty007@gmail.com>
2020-12-28 14:04:56 +0500

    fetchLeadProducts js updated

--- custom/modules/AOS_Quotes/fetchLeadProduct.js
+++ custom/modules/AOS_Quotes/fetchLeadProduct.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  var full_url = document.URL;
+  var url_array = full_url.split("="); // Split the string into an array with / as separator
+  var last_segment = url_array[url_array.length - 1]; // Get the last part of the array (-1)
+  // var lead_id = last_segment;
   $.ajax({
     url: 'index.php?module=AOS_Quotes&action=GetLeadProducts&sugar_body_only=true&lead_id=' + last_segment,
     datatype: 'JSON',
