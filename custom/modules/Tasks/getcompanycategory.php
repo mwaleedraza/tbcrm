73fe5126caf15b49cf48a237e03b387bac8c8b07
aizazhasan <hasanaizaz786@gmail.com>
2022-03-11 17:05:58 +0500

    ]

--- custom/modules/Tasks/getcompanycategory.php
+++ custom/modules/Tasks/getcompanycategory.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 
 $id=$_REQUEST['id'];
@@ -6,13 +6,4 @@ $id=$_REQUEST['id'];
 $companybean=BeanFactory::getBean('Accounts',$id);
 
 
-=======
-<?php
-
-$id=$_REQUEST['id'];
-
-$companybean=BeanFactory::getBean('Accounts',$id);
-
-
->>>>>>> f76da31ca3d45dc2af3729a157f7904548c77879
 echo $companybean->account_type;
--- custom/modules/Tasks/getcurrentuser.php
+++ custom/modules/Tasks/getcurrentuser.php
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
 <?php
 
 global $current_user;
 
-=======
-<?php
-
-global $current_user;
-
->>>>>>> f76da31ca3d45dc2af3729a157f7904548c77879
 echo $current_user->id;
