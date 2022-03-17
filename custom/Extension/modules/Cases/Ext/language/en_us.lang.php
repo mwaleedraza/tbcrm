1a9886e3e21de3e0860a2b9fe79bd6ec6f7a10d9
aizazhasan <hasanaizaz786@gmail.com>
2022-03-17 16:28:49 +0500

    [update]label in cases module

--- custom/Extension/modules/Cases/Ext/language/en_us.lang.php
+++ custom/Extension/modules/Cases/Ext/language/en_us.lang.php
@@ -1,8 +1,9 @@
 <?php
 
 $mod_strings = array (
-   'LBL_TICKET_NO' => 'Ticket #',
-   'LBL_CONTACT_NAME' => 'Client Name',
-   'LBL_PRODUCT_NAME' => 'Product/Solution',
-   'LBL_CLOSURE_DATE' => 'Closure Date',
+   'LBL_TICKET_NO ' => 'Ticket #',
+   'LBL_CONTACT_NAME ' => 'Client Name',
+   'LBL_PRODUCT_NAME ' => 'Product/Solution',
+   'LBL_CLOSURE_DATE ' => 'Closure Date',
+   ''
 );
