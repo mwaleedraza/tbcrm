714be07c984d82585e07d4ac3e70beeefedf4b3a
aizazhasan <hasanaizaz786@gmail.com>
2022-05-23 01:31:57 -0700

    dropdown changwe

--- custom/Extension/application/Ext/Language/en_us.account_type.php
+++ custom/Extension/application/Ext/Language/en_us.account_type.php
@@ -3,5 +3,6 @@ $app_list_strings['account_type_dom'] = array(
     '' => '',
     'Client' => 'Client',
     'Vendor' => 'Vendor',
-    'Distributor' => 'Distributor',
+    'Internal' => 'Internal',
+    'Distributor' => 'Distributor'
 );
