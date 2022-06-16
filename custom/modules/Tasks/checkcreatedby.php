e4b3d4c09e79f0015e33530c7f6ee0315ccdc860
aizazhasan <hasanaizaz786@gmail.com>
2022-06-16 13:37:29 +0500

    admin can also close task

--- custom/modules/Tasks/checkcreatedby.php
+++ custom/modules/Tasks/checkcreatedby.php
@@ -4,7 +4,7 @@
 global $current_user;
 
 $taskbean=BeanFactory::getBean('Tasks',$_REQUEST['record_id']);
-if($current_user->id==$taskbean->created_by)
+if($current_user->id==$taskbean->created_by ||$current_user->is_admin==1)
 {
     echo 1;
 }else{
