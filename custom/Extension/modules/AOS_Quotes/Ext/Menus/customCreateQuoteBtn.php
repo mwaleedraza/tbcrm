996f6c49eb6e2570e7d8435377a4232c3de81660
junaidgemini <junaid.gemini24@gmail.com>
2023-06-20 18:28:09 +0500

    update menu quotes

--- custom/Extension/modules/AOS_Quotes/Ext/Menus/customCreateQuoteBtn.php
+++ custom/Extension/modules/AOS_Quotes/Ext/Menus/customCreateQuoteBtn.php
@@ -4,6 +4,6 @@ global $module_menu;
 // unset original create quote button
 unset($module_menu['0']);
 // custom create quote button
-$module_menu['0']=Array("index.php?module=AOS_Quotes&action=jd_customQuote&return_module=Leads&return_action=DetailView", "Create Quote", "Create", "AOS_Quotes");
+$module_menu['0']=Array("index.php?module=AOS_Quotes&action=jd_customQuote&return_module=AOS_Quotes&return_action=DetailView", "Create Quote", "Create", "AOS_Quotes");
 
 ?>
