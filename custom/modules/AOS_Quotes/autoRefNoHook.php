06e9102d37232ad86d49555c7589d906c0108496
junaidgemini <junaid.gemini24@gmail.com>
2023-11-27 12:38:14 +0500

    allow shot code to update in ref number even in edit

--- custom/modules/AOS_Quotes/autoRefNoHook.php
+++ custom/modules/AOS_Quotes/autoRefNoHook.php
@@ -4,13 +4,13 @@ if (!defined('sugarEntry'))
 
 class autoRefNoClass{
     function autoRefNoMethod($bean, $event, $arguments){
-		if(empty($bean->fetched_row)){
+		// if(empty($bean->fetched_row)){
             $product_short_code = $bean->product_short_code;
             $date = $bean->date_entered;
             $date = date("ymd", strtotime($date));
             $version = 'V1';
             $refNo = $bean->number . '-' . $product_short_code . '-' . $date . '-' . $version;
             $bean->referencenumber = $refNo;
-        }
+        // }
     }
 }
